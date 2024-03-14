7f90c7da20087d6ee8eca2c7f5a9df0e1eaeb567
Bulat <konnor1500@icloud.com>
2024-03-14 21:47:46 +0300

    Design ready

--- app/src/main/java/com/example/kotlinpracticemirea/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpracticemirea/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.kotlinpracticemirea
 
-import android.os.Binder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kotlinpracticemirea.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
